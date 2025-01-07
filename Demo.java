909fcda39698e906100400186957de09b3de2c34
Sandeep Mahto <san14deep88@gmail.com>
2025-01-07 11:36:05 +0530

    new feature has been added in feature branch

--- Demo.java
+++ Demo.java
@@ -4,6 +4,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello sandeep");
         Scanner sc = new Scanner(System.in);
-        
+        String  s= sc.nextLine();
+        System.out.println(s);
     }
 }
